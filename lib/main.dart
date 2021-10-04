e14f74d72d5a49ce6c548be106a2cc7ab8a0748f
Mark Vincent Atinon <mark_atinon@yahoo.com>
2021-10-04 15:56:25 +0800

    semin final

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,12 @@ class FirstPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(title: Text('Exercise 1'), backgroundColor: Colors.blue),
+        appBar: AppBar(
+          title: Text(
+            'Exercise 1',
+          ),
+          backgroundColor: Colors.blue,
+        ),
         body: SafeArea(
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
@@ -20,18 +25,22 @@ class FirstPage extends StatelessWidget {
               Center(
                 child: Image(
                   image: NetworkImage(
-                    'https://image.freepik.com/free-vector/group-people-talking-each-other_179970-994.jpg',
-                  ),
+                      'https://image.freepik.com/free-vector/group-people-talking-each-other_179970-994.jpg'),
+                  width: 180,
+                  height: 180,
                 ),
               ),
               Text(
-                  'Written By : Mark Vincent Atinon, Jed Adrian Denosta, Natalie Jane Pacificar'),
+                'Written By : Mark Vincent Atinon, Jed Adrian Denosta, Natalie Jane Pacificar',
+                style: TextStyle(fontSize: 10.0),
+              ),
               ElevatedButton(
-                child: const Text('Open route'),
+                child: const Text('Log in Page'),
                 onPressed: () {
                   Navigator.push(
                     context,
-                    MaterialPageRoute(builder: (context) => const SecondRoute()),
+                    MaterialPageRoute(
+                        builder: (context) => const SecondRoute()),
                   );
                 },
               ),
@@ -48,6 +57,55 @@ class SecondRoute extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          'Exerise 2',
+        ),
+      ),
+      body: SafeArea(
+        child: Column(
+          children: <Widget>[
+            SizedBox(
+              height: 150.0,
+            ),
+            Center(
+              child: Text(
+                'Simple Log in Page Flutter',
+                style: TextStyle(fontSize: 20, color: Colors.blue),
+              ),
+            ),
+            SizedBox(
+              height: 60.0,
+            ),
+            TextField(
+              decoration: InputDecoration(
+                labelText: 'Email',
+                labelStyle: TextStyle(fontSize: 20.0),
+                filled: true,
+              ),
+            ),
+            SizedBox(
+              height: 30.0,
+            ),
+            TextField(
+              decoration: InputDecoration(
+                labelText: 'Password',
+                labelStyle: TextStyle(fontSize: 20.0),
+                filled: true.
+              ),
+            ),
+            SizedBox(
+              height: 30.0,
+            ),
+            ElevatedButton(
+                child: const Text('Log in'),
+                onPressed: () {
+                },
+              ),
+          ],
+        ),
+      ),
+    );
   }
 }
